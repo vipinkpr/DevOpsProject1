0e41a770d0c57b13159cf269e9c4d3e474e0299c
vipinkpr <vipinkpr@hotmail.com>
2021-04-30 15:46:07 +0530

    Second Commit

--- DevOpsProject1/App_Start/FilterConfig.cs
+++ DevOpsProject1/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace DevOpsProject1
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute()); 
         }
     }
 }
