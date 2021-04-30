379765d619629330744e972e88a169f8b14bd2ed
vipinkpr <vipinkpr@hotmail.com>
2021-04-30 15:40:02 +0530

    Initial Commit

--- DevOpsProject1/App_Start/BundleConfig.cs
+++ DevOpsProject1/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace DevOpsProject1
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
